d169c8c1bd78cd4cc82a92b09f7f77efbac6daff
DaiQing Liao <49213588+dqing-dq@users.noreply.github.com>
2021-04-07 17:43:41 -0400

    added decision dispatcher

--- index.js
+++ index.js
@@ -32,21 +32,164 @@ function handleStart(request, response) {
   response.status(200).send('ok')
 }
 
+
+
+
 function handleMove(request, response) {
   var gameData = request.body
 
-  var possibleMoves = ['up', 'down', 'left', 'right']
-  var move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)]
+  //var possibleMoves = ['up', 'down', 'left', 'right']
+  //var move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)]
+  
+  var generatedMove = 'up';
+  var possibleMoves = ['up', 'down', 'left', 'right'];
+  
+  var grid = createMap(request.board.width, request.board.height);
+  var snake = [];
+  var food = [];
+  
+  var cornerMove = checkCorners(req.body);
+  var dangerousFlag = false;
 
-  console.log('MOVE: ' + move)
-  response.status(200).send({
-    move: move
-  })
+  
+  //WILL NEED TO MAKE THIS FUNCTION
+  modifyGrid(grid);
+  
+  
+  //check if in a corner
+  if (snake[0].x == 0){
+    if (snake[0].y == 0){
+      if (snake[1].x == 0){
+        response = { "move": "right" }
+        return;
+      } else {
+        response = { "move": "down" }
+        return;
+      }
+    } else if (snake[0].y == request.board.height - 1){
+      if (snake[1].x == 0){
+        response = { "move": "right" }
+        return;
+      } else {
+        response = { "move": "up" }
+        return;
+      }
+    }
+  } else if (snake[0].x == request.board.width - 1){
+    if (snake[0].y == 0){
+      if (snake[1].y == 0){
+        response = { "move": "up" }
+        return;
+      } else {
+        response = { "move": "left" }
+        return;
+      }
+    } else if (snake[0].y == request.board.height - 1){
+      if (snake[1].x == request.board.height -1){
+        response = { "move": "left" }
+        return;
+      } else {
+        response = { "move": "down" }
+        return;
+      }
+    }
+  }
+  
+  
+  
+  //Compute the path to both your tail and the closest piece of food on the board
+  //this should be a*?
+  var closestFood = findClosestFood(food);
+  
+  var tempMoveFood = aStar(grid, snake[0], closestFood);
+  
+  //If you are hungry and there exists a path to the closest piece of food, make the best move to get closer to the food.
+  //40 is arbitrary we can change
+  if (health < 40 && tempMoveFood != null){
+    return moveToRequest(tempMoveFood);
+  }
+  
+  //If you are not hungry or there doesn’t currently exist a path to the food, check to see if there is a path to your tail
+  //if there is, then make the best move to get closer to your tail.
+  var tempMoveTail = aStar(grid, snake[0], snake[snake.length - 1]);
+  if (tempMoveTail != null){
+    return moveToRequest(tempMoveTail);
+  }
+  
+  //If there isn’t a path to your tail, make a move in the direction with the most “promise.”
+  var tempMoveFlood = floodFill();
+  if (tempMoveFlood != null){
+    return movetoRequest(tempMoveFlood);
+  }
+
+  //if we reach here what do we do?
+  return { "move": "up" }
 }
+
+
 
 function handleEnd(request, response) {
   var gameData = request.body
 
   console.log('END')
   response.status(200).send('ok')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// custom code
+
+//TODO:
+function modifyGrid(grid){
+  //need to add snakes, food, etc.
+}
+function findClosestFood(food){
+  //find closest food from array
+}
+
+
+
+
+//DONE:
+function moveToRequest(move){
+  if (move == "up"){
+    return { "move": "up" }
+  } else if (move == "down"){
+    return { "move": "down" }
+  } else if (move == "right"){
+    return { "move": "right" }
+  } else if (move == "left"){
+    return { "move": "left" }
+  } else {
+    console.log("error in move");
+  }
+}
+
+function createMap(columnCount, rowCount) {
+   const map = [];
+   for (let x = 0; x < columnCount; x++) {
+       map[x] = [];
+       for (let y = 0; y < rowCount; y++) {
+          addCell(map, x, y);
+       }
+   }
+   return map;
+}
+
+function replaceCell(map, x, y, item) {
+    map[x][y] = item; // create a new object on x and y
 }
