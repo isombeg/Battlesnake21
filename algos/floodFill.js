ebad1acf29c286593f825a33c61cb1ed00e52cc6
Irene Park <irenepark1221@gmail.com>
2021-04-08 14:01:20 -0400

    floodFills and tailTrim updated

--- algos/floodFill.js
+++ algos/floodFill.js
@@ -1,14 +1,30 @@
-// var prevColor;
-// var node;
-// var replacementColor;
-var size = 8; 
+var size; 
+var x;
+var y;
+
+
+function isBeside(currPos,enemyPos){
+    if (currPos.col + 1 == enemyPos)
+        return false;
+    if (currPos.col -1 == enemyPos)
+        return false;
+    if (currPos.row +1 == enemyPos)
+        return false;
+    if (currPos.row - 1 == enemyPos)
+        return false;
+    return true;
+}
+
 
 function floodFill(x, y, prevColor, replacementColor){
+    //base case :
+    //if current color is not prevColor return;
+    //if current color is replacementColor return;
     if (x<0 || x >=size || y<0 || y>=size){
         return;
     } 
-    //if current color is not prevColor return;
-    //if current color is replacementColor return;
+
+    //directions
     floodFill(x+1,y,prevColor,replacementColor);
     floodFill(x-1,y,prevColor,replacementColor);
     floodFill(x,y+1,prevColor,replacementColor);
@@ -16,7 +32,3 @@ function floodFill(x, y, prevColor, replacementColor){
 
 }
 
-
-// function isBeside(x,y){
-    
-// }
