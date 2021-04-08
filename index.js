77e12ede5694d7f341c775ad00f346d81346b749
Guy-Jacques Isombe <guy-jacquesisombe@Guy-Jacquess-MacBook-Pro.local>
2021-04-07 20:43:53 -0400

    finished writing server. all happy paths work

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 
+const {
+  handleIndex,
+  handleStart,
+  handleMove,
+  handleEnd
+} = require('./controllers/handleEvent');
+
 const PORT = process.env.PORT || 3000
 
 const app = express()
@@ -14,39 +21,4 @@ app.post('/end', handleEnd)
 app.listen(PORT, () => console.log(`Battlesnake Server listening at http://127.0.0.1:${PORT}`))
 
 
-function handleIndex(request, response) {
-  var battlesnakeInfo = {
-    apiversion: '1',
-    author: '',
-    color: '#888888',
-    head: 'default',
-    tail: 'default'
-  }
-  response.status(200).json(battlesnakeInfo)
-}
 
-function handleStart(request, response) {
-  var gameData = request.body
-
-  console.log('START')
-  response.status(200).send('ok')
-}
-
-function handleMove(request, response) {
-  var gameData = request.body
-
-  var possibleMoves = ['up', 'down', 'left', 'right']
-  var move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)]
-
-  console.log('MOVE: ' + move)
-  response.status(200).send({
-    move: move
-  })
-}
-
-function handleEnd(request, response) {
-  var gameData = request.body
-
-  console.log('END')
-  response.status(200).send('ok')
-}
